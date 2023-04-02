7c1fd1eb52a49638729b0daf72eb5aef09a8aa1a
V <viziong@yandex.com>
2023-04-02 23:15:59 +0100

    1.2

--- repo/Vizion.Wizard/uservar.py
+++ repo/Vizion.Wizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = 'Vizion Wizard'
 BUILDERNAME = 'VizionWizard'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'https://viziong.github.io/vizion_wizard/resources/text/builds.txt'
+BUILDFILE = 'https://viziong.github.io/vizion.repo/txt/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 10
