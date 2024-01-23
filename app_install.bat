2f55818fa9d71f09f6d9f9a17ba116153b862194
Vizion <tito_goa@hotmail.com>
2024-01-23 17:38:13 +0000

    Add files via upload

---
+++ app_install.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/VizionG/9f1cd597f0842d2fdff2f119b6fb02fa/raw'))"
